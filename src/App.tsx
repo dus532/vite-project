61cd775fd41f92a7c51a221ced5b20d409cddca3
정연진 <dus532@naver.com>
2023-03-08 16:10:20 +0900

    03 State로 변경하여 표시

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { useState } from 'react';
 import './App.css';
 import TodoListItem from './TodoListItem';
 
-// 1. data를 뿌려주는 가짜 LIST
-const LIST = [{ title: '빨래하기' }, { title: '세차하기' }];
-
 function App() {
+  const [LIST, setLIST] = useState([
+    { title: '빨래하기' },
+    { title: '세차하기' },
+  ]);
+
   return (
     <div className='App'>
       <button>추가하기</button>
