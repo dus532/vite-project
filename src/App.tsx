1338cbbb49a26e4fec6b120a3593c97e8523ab51
정연진 <dus532@naver.com>
2023-03-08 17:58:11 +0900

    10 TodoList 제작

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { ReactNode } from 'react';
 import './App.css';
 import TodoListItem from './TodoListItem';
 import { useTodoList } from './useTodoList';
@@ -10,17 +11,36 @@ function App() {
     <div className='App'>
       <input value={value} onChange={handleValueChange} />
       <button onClick={onAdd}>추가하기</button>
-      <div className='todolist'>
-        {list.map((item, index) => (
-          <TodoListItem
-            key={index}
-            item={item}
-            onDelete={onDelete}
-            onToggle={onToggle}
-            index={index}
-          />
-        ))}
-      </div>
+      <TodoList
+        data={list}
+        renderItem={({ item, index }) => {
+          return (
+            <TodoListItem
+              key={index}
+              item={item}
+              onDelete={onDelete}
+              onToggle={onToggle}
+              index={index}
+            />
+          );
+        }}
+      />
+    </div>
+  );
+}
+
+function TodoList<T>({
+  data,
+  renderItem,
+}: {
+  data: T[];
+  renderItem: ({ item, index }: { item: T; index: number }) => ReactNode;
+}) {
+  return (
+    <div className='todolist'>
+      {data?.map((item, index) => {
+        return renderItem({ item, index });
+      })}
     </div>
   );
 }
