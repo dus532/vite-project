24a080fad4d97b2b7668c6e05d7e397950c45235
정연진 <dus532@naver.com>
2023-03-08 16:13:14 +0900

    04 Input 제작 후 제어 컴포넌트로 변경 ( state 연결 )

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,26 @@ import './App.css';
 import TodoListItem from './TodoListItem';
 
 function App() {
-  const [LIST, setLIST] = useState([
+  const [list, setList] = useState([
     { title: '빨래하기' },
     { title: '세차하기' },
   ]);
 
+  const [value, setValue] = useState('');
+  console.log(value);
+
   return (
     <div className='App'>
+      <input
+        value={value}
+        onChange={(e) => {
+          setValue(e.target.value);
+        }}
+      />
       <button>추가하기</button>
       <div className='todolist'>
-        {LIST.map((item, index) => (
-          <TodoListItem item={item} index={index} />
+        {list.map((item, index) => (
+          <TodoListItem key={index} item={item} index={index} />
         ))}
       </div>
     </div>
