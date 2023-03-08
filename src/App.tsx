a115abf8da3bf919ca149ccd293abcef3e4dee4e
정연진 <dus532@naver.com>
2023-03-08 16:09:43 +0900

    02 컴포넌트 제작 및 컴포넌트로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css';
+import TodoListItem from './TodoListItem';
 
 // 1. data를 뿌려주는 가짜 LIST
 const LIST = [{ title: '빨래하기' }, { title: '세차하기' }];
@@ -9,16 +10,7 @@ function App() {
       <button>추가하기</button>
       <div className='todolist'>
         {LIST.map((item, index) => (
-          <div className='todolist_item'>
-            <div className='todolist_item_name'>{index + 1}</div>
-            <div className='todolist_item_title'>{item.title}</div>
-            <div className='todolist_item_button'>
-              <button>수정</button>
-            </div>
-            <div className='todolist_item_button'>
-              <button>삭제</button>
-            </div>
-          </div>
+          <TodoListItem item={item} index={index} />
         ))}
       </div>
     </div>
